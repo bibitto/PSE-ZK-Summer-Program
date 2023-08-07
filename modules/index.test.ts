fa11b1aa451c498d80474aad74bebe418276106c
bibitto <bibitto.crypto@gmail.com>
2023-08-08 00:42:55 +0900

    remove ts packages

--- modules/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { modularCalculator } from "./module1/modularCalculator";
-
-test("module1_modularCalculator", () => {
-  expect(modularCalculator("+", 10, 15, 12)).toBe(1);
-  expect(modularCalculator("-", 10, 15, 12)).toBe(7);
-  expect(modularCalculator("*", 10, 15, 12)).toBe(6);
-});
